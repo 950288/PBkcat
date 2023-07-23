f6651cc398c2f3efed7c2ac1f1ce0a43f9154a7f
950288 <950288s@gmail.com>
2023-07-23 10:58:23 +0800

    create 2 files and update 1 file

--- preprocess/proteinBERT.py
+++ preprocess/proteinBERT.py
@@ -3,7 +3,7 @@ import pickle
 import json
 # from proteinbert.conv_and_global_attention_model import get_model_with_hidden_layers_as_outputs
 
-pretrained_model_generator, input_encoder = load_pretrained_model(local_model_dump_dir = "D:\\github\\Machine-Learning-2023\\test\\bert\\" , local_model_dump_file_name = 'epoch_92400_sample_23500000.pkl')
+pretrained_model_generator, input_encoder = load_pretrained_model(local_model_dump_dir = ".\\preprocess\\" , local_model_dump_file_name = 'epoch_92400_sample_23500000.pkl')
 
 model = pretrained_model_generator.create_model(seq_len = 512)
 # model = get_model_with_hidden_layers_as_outputs(pretrained_model_generator.create_model(seq_len = 512))
@@ -18,7 +18,7 @@ for data in Kcat_data :
     _ , global_representation = model.predict(input_ids)
     global_representations.append(global_representation)
     # local_representations, global_representation = model.predict(input_ids)
-    print(len(global_representations) + 1 , '/' , len(Kcat_data) , end = '\n')
+    print(len(global_representations) , '/' , len(Kcat_data) , end = '\n')
 
 # print(local_representations.shape) # (20, 512, 26)
 # print(global_representation.shape) # (20, 8943)
