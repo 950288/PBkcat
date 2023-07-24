f15f447feb3ed5523de9f9921676f26c6fc60af6
950288 <950288s@gmail.com>
2023-07-24 19:34:58 +0800

    update proteinBERT.py

--- preprocess/proteinBERT.py
+++ preprocess/proteinBERT.py
@@ -12,16 +12,17 @@ model = pretrained_model_generator.create_model(seq_len = 512)
 
 global_representations = list()
 
-with open('./data/test.json', 'r') as infile :
+with open('./data/Kcat_combination_0918.json', 'r') as infile :
     Kcat_data = json.load(infile)
 
 for data in Kcat_data :
     input_ids = input_encoder.encode_X(data['Sequence'] , 512)
     _ , global_representation = model.predict(input_ids)
-    (global_representation_a,global_representation_b) = global_representation.shape
+    (global_representation_a,_) = global_representation.shape
     global_representation_c = 512-global_representation_a
     global_representation_1 = np.pad(global_representation,((0,global_representation_c),(0,0)),'constant',constant_values = (0,0))
     global_representations.append(global_representation_1)
+    print(global_representation_1)
     print(len(global_representations) , '/' , len(Kcat_data) , end = '\n')
 
 np.save('./data/global_representations.npy', global_representations)
