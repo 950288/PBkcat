4e5c1dd275c3e4c3b0b066745c52d6e15557c051
950288 <950288s@gmail.com>
2023-07-27 17:00:36 +0800

    update model.py

--- model/model.py
+++ model/model.py
@@ -94,7 +94,7 @@ class Trainer(object):
         trainPredict = torch.stack(trainPredict).detach().cpu().numpy()
         rmse_train = np.sqrt(mean_squared_error(trainCorrect, trainPredict))
         r2_train = r2_score(trainCorrect, trainPredict)
-        print('Test RMSE: %.4f , R2: %.4f' %(rmse_train, r2_train))
+        print('Train RMSE: %.4f , R2: %.4f' %(rmse_train, r2_train))
         return loss_total, rmse_train, r2_train
 
 class Tester(object):
