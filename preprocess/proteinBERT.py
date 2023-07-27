e6c811e406297369d7a045ef40f7ee65398752d2
950288 <950288s@gmail.com>
2023-07-27 11:03:14 +0800

    update proteinBERT.py

--- preprocess/proteinBERT.py
+++ preprocess/proteinBERT.py
@@ -28,21 +28,24 @@ max_len = 0
 for i , data in enumerate(Kcat_data) :
     sequences.append(str(data['Sequence']))
     max_len = max(max_len , len(data['Sequence']))
-    # input_ids = input_encoder.encode_X(data['Sequence'] , 16)
-    # local_representation , global_representation = model.predict(input_ids)
-    # # global_representations.append(global_representation)
-    # local_representations.append(local_representation)
-    # if i % 100 == 0 :
-    #     print(i + 1 , '/' , len(Kcat_data) , end = '\n')
 
 max_len += 2
-print(max_len)
 
 model = pretrained_model_generator.create_model(max_len)
 
 input_ids = input_encoder.encode_X(sequences, max_len)
 
-local_representations , global_representations = model.predict(input_ids, batch_size=16)
+step = 16
+for i in range(0, len(input_ids), step):
+    input_ids_ = input_ids[i:i+step]
+    local_representations_ , global_representations_ = model.predict(input_ids_, batch_size=16)
+    if local_representations:
+        local_representations = np.concatenate((local_representations, local_representations_), axis=0)
+        global_representations = np.concatenate((global_representations, global_representations_), axis=0)
+    else:
+        local_representations = local_representations_
+        global_representations = global_representations_
+    
 
 print(local_representations.shape, global_representations.shape)
 save_array(global_representations, './data/global_representations.pickle')
