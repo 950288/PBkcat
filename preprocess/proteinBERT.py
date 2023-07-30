5864606a53bd9e12691571ab5a6bf9661f597811
950288 <950288s@gmail.com>
2023-07-30 19:04:05 +0800

    update proteinBERT.py

--- preprocess/proteinBERT.py
+++ preprocess/proteinBERT.py
@@ -11,7 +11,7 @@ pretrained_model_generator, input_encoder = load_pretrained_model(local_model_du
 global_representations = list()
 local_representations = list()
 
-with open('./data/test2.json', 'r') as infile :
+with open('./data/Kcat_combination_0918.json', 'r') as infile :
     Kcat_data = json.load(infile)
 
 def dump_dictionary(dictionary, filename):
@@ -39,7 +39,7 @@ for i in range(0, len(sequences), step):
     sequences_ = sequences[i:i+step]
     input_ids = input_encoder.encode_X(sequences_, max_len)
     _ , global_representations_ = model.predict(input_ids, batch_size=16)
-    if len(local_representations) != 0:
+    if len(global_representations_) != 0:
         # local_representations = np.concatenate((local_representations, local_representations_), axis=0)
         global_representations = np.concatenate((global_representations, global_representations_), axis=0)
     else:
