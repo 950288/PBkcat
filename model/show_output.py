0bef05c5932491c55c91bd552ca5f118fd3690bb
950288 <950288s@gmail.com>
2023-08-01 19:40:08 +0800

    update show_output.py

--- model/show_output.py
+++ model/show_output.py
@@ -10,26 +10,25 @@ R2_train = data['R2_train']
 R2_test = data['R2_test']
 Lr_values = data['Lr']
 
-# Plotting R2_train and R2_test values
-plt.figure(figsize=(10, 6))
-plt.plot(epochs, R2_train, label='R2_train', marker='o', linestyle='-', color='blue')
-plt.plot(epochs, R2_test, label='R2_test', marker='o', linestyle='-', color='red')
-plt.xlabel('Epochs')
-plt.ylabel('R2 Value')
-plt.title('R2_train and R2_test over Epochs')
-plt.legend()
+# Create a figure and axis for the plot
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+# Plotting R2_train and R2_test values on the first Y-axis (ax1)
+ax1.plot(epochs, R2_train, label='R2_train', marker='o', linestyle='-', color='blue')
+ax1.plot(epochs, R2_test, label='R2_test', marker='o', linestyle='-', color='red')
+ax1.set_xlabel('Epochs')
+ax1.set_ylabel('R2 Value')
+ax1.set_title('R2_train and R2_test over Epochs')
+ax1.legend()
+
+# Create a second Y-axis for Lr values (ax2)
+ax2 = ax1.twinx()
+ax2.plot(epochs, Lr_values, label='Lr', marker='o', linestyle='-', color='yellow')
+ax2.set_ylabel('Lr Value')
+
+# Adjust the Y-axis limits for R2 values
+ax1.set_ylim(-0.2, 1)
+
+# Show the plot
 plt.grid(True)
-
-# Create a new figure for Lr values
-plt.figure(figsize=(10, 6))
-plt.plot(epochs, Lr_values, label='Lr', marker='o', linestyle='-', color='yellow')
-plt.xlabel('Epochs')
-plt.ylabel('Lr Value')
-plt.title('Lr over Epochs')
-plt.legend()
-plt.grid(True)
-
-# Set the Y-axis limits for R2 and Lr plots
-plt.ylim(-0.2, 1)
-
 plt.show()
