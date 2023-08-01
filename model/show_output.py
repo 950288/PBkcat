76e3909900d068adeeeb0bcd07642b69c89e94a4
950288 <950288s@gmail.com>
2023-08-01 19:33:50 +0800

    update show_output.py

--- model/show_output.py
+++ model/show_output.py
@@ -8,25 +8,28 @@ data = pd.read_csv('./model/output/Kcat-MAEs.csv')
 epochs = data['epoch']
 R2_train = data['R2_train']
 R2_test = data['R2_test']
-Lr = data['Lr']
+Lr_values = data['Lr']
 
-# Plotting the R2_train and R2_test values
-R2, Lr = plt.subplots()
+# Plotting R2_train and R2_test values
 plt.figure(figsize=(10, 6))
 plt.plot(epochs, R2_train, label='R2_train', marker='o', linestyle='-', color='blue')
-R2.plot(epochs, R2_test, label='R2_test', marker='o', linestyle='-', color='red')
-R2.xlabel('Epochs')
-R2.ylabel('R2 Value')
+plt.plot(epochs, R2_test, label='R2_test', marker='o', linestyle='-', color='red')
+plt.xlabel('Epochs')
+plt.ylabel('R2 Value')
 plt.title('R2_train and R2_test over Epochs')
 plt.legend()
 plt.grid(True)
 
-# Lr.figure(figsize=(10, 6))
-Lr.plot(epochs, Lr, label='Lr', marker='o', linestyle='-', color='yellow')
-Lr.xlabel('Epochs')
-Lr.ylabel('R2 Value')
+# Create a new figure for Lr values
+plt.figure(figsize=(10, 6))
+plt.plot(epochs, Lr_values, label='Lr', marker='o', linestyle='-', color='yellow')
+plt.xlabel('Epochs')
+plt.ylabel('Lr Value')
+plt.title('Lr over Epochs')
+plt.legend()
+plt.grid(True)
 
-# Set the Y-axis limits to 0 and 100
+# Set the Y-axis limits for R2 and Lr plots
 plt.ylim(-0.2, 1)
 
 plt.show()
