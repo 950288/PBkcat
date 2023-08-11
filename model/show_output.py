312c93185306e0f46d7a2e7f9a9d2ae0f76cf63b
950288 <950288s@gmail.com>
2023-08-11 09:22:44 +0800

    update show_output.py

--- model/show_output.py
+++ model/show_output.py
@@ -9,8 +9,8 @@ epochs = data['epoch']
 R2_train = data['R2_train']
 R2_test = data['R2_dev']
 Lr_values = data['Lr']
-LOSS_train = data['LOSS_train']
-LOSS_dev = data['LOSS_dev']
+RMSE_train = data['RMSE_train']
+RMSE_dev = data['RMSE_dev']
 
 
 # Create a figure and axis for the plot
@@ -36,22 +36,24 @@ ax1.set_ylim(-0.2, 1)
 plt.grid(True)
 plt.show()
 
-# Create anotner plot for loss and LR
+# Create anotner plot for RMSE and LR
 fig, ax1 = plt.subplots(figsize=(10, 6))
 
-# Plotting LOSS_train and LOSS_dev values on the first Y-axis (ax1)
-ax1.plot(epochs, LOSS_train, label='LOSS_train', marker='o', linestyle='-', color='blue')
-ax1.plot(epochs, LOSS_dev, label='LOSS_dev', marker='o', linestyle='-', color='red')
+# Plotting RMSE_train and RMSE_dev values on the first Y-axis (ax1)
+ax1.plot(epochs, RMSE_train, label='RMSE_train', marker='o', linestyle='-', color='blue')
+ax1.plot(epochs, RMSE_dev, label='RMSE_dev', marker='o', linestyle='-', color='red')
 ax1.set_xlabel('Epochs')
-ax1.set_ylabel('LOSS Value')
-ax1.set_title('LOSS_train and LOSS_dev over Epochs')
+ax1.set_ylabel('RMSE Value')
+ax1.set_title('RMSE_train and RMSE_dev over Epochs')
 ax1.legend()
 
 # Create a second Y-axis for Lr values (ax2)
 ax2 = ax1.twinx()
+ax2.plot(epochs, Lr_values, label='Lr', linestyle='-', color='green')
+ax2.set_ylabel('Lr Value')
 
-# Adjust the Y-axis limits for LOSS values
-ax1.set_ylim(0, 0.5)
+# Adjust the Y-axis limits for RMSE values
+# ax1.set_ylim(0, 0.5)
 
 # Show the plot
 plt.grid(True)
