85aeea124ccdf9ce7205fd140ce949cf643168f0
95028 <2779307196@qq.com>
2023-08-11 09:20:44 +0800

    update .gitignore and train_model.py

--- model/train_model.py
+++ model/train_model.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     """model_name used to save the model and MAEs"""
 
-    model_name = 'Kcat_810'
+    model_name = 'Kcat_811'
 
     """hyperparameters"""
     args = {
